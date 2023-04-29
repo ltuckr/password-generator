c3255de22dfbfef213bdafabb99806e6b9628e0d
ltuckr <l.tucker.rdh@gmail.com>
2023-04-29 11:41:56 -0400

    added basic password variables: alphabet UC/lc numbers and symbols.

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
-// Assignment Code
+var uppercase: [A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z]
+var lowercase:[a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z]
+var numbers:[0,1,2,3,4,5,6,7,8,9,0]
+var symbols:[! , @, #, $, &, ?,]
+
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
