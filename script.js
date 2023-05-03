8cc824d677ce2b19c9bdfb3833fc1c54816a19dd
ltuckr <l.tucker.rdh@gmail.com>
2023-05-03 18:05:24 -0400

    added changes to prompts for each criteria

--- script.js
+++ script.js
@@ -6,9 +6,13 @@ var generateBtn = document.querySelector("#generate");
 // user criteria prompts: length and type
 function writePassword() {
   // PROMPT:  How long is this password?
-  var passLength = (prompt)("Please enter the numeric value required for your password's length, from 8-128 characters.");
+  var Length = (prompt)("Please enter the numeric value required for your password's length, from 8-128 characters.");
   // PROMPT: What kinds of characters should be included in this password? 
-  var passChars =  (prompt)("Please specify the types of characters required in your password: uppercase, lowercase, numbers or symbols."); 
+  var upper=  (prompt)("Does the password require uppercase letters?"); 
+  var lower=  (prompt)("Does the password require lowercase letters?"); 
+  var numbers=  (prompt)("Does the password require numbers?"); 
+  var symbols=  (prompt)("Does the password require symbols); 
+
 
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
