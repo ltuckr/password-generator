398a6b6e29e1d7b4b4d0650688035bcb42e35fe2
ltuckr <l.tucker.rdh@gmail.com>
2023-05-03 14:46:24 -0400

    fixed prompt for number. Changed 'howMany' to 'number' due to console error.

--- script.js
+++ script.js
@@ -6,9 +6,9 @@ var generateBtn = document.querySelector("#generate");
 // Write password to the #password input
 function writePassword() {
   // PROMPT:  How long is this password?
-  var passLength = howMany(prompt)("Please enter the numeric value required for your password's length, from 8-128 characters.");
+  var passLength = Number(prompt)("Please enter the numeric value required for your password's length, from 8-128 characters.");
   // PROMPT: What kinds of characters should be included in this password? 
-  var passChars = whatKind(prompt)("Please specify the types of characters required in your password: uppercase, lowercase, numbers or symbols."); 
+  var passChars =  (prompt)("Please specify the types of characters required in your password: uppercase, lowercase, numbers or symbols."); 
 
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
