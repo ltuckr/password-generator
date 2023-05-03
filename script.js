7b6071a93b498d238674768e5aca5ecdd6a22eff
ltuckr <l.tucker.rdh@gmail.com>
2023-05-03 12:00:39 -0400

    Prompt completed

--- script.js
+++ script.js
@@ -1,25 +1,25 @@
 // array of password creation character options// 
-var characters = {
+var charsAll = {
   upperCase: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
   lowerCase: "abcdefghijklmnopqrstuvwxyz",
   numbers: "1234567890",
   symbols: "!@#$%^&*_+=?",
 };
 
-// Assignment Code
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
   // PROMPT:  How long is this password?
-  var passLength = howManychars(prompt)("Please enter the numeric value required for your password's length, from 8-128 characters.");
-  // PROMPT: What kinds of characters in this password? 
-  var 
+  var passLength = howMany(prompt)("Please enter the numeric value required for your password's length, from 8-128 characters.");
+  // PROMPT: What kinds of characters should be included in this password? 
+  var passChars = whatKind(prompt)("Please specify the types of characters required in your password: uppercase, lowercase, numbers or symbols."); 
 
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+}
+
 
 }
 
