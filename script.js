f65c3fd02fc2c2a915a9bd3bea7afcec0b5cd431
ltuckr <l.tucker.rdh@gmail.com>
2023-05-04 12:20:53 -0400

    created for loop for numerical value

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // array of password creation character options// 
 
 
-var generateBtn = document.querySelector("#generate").addEventListener("click",writePassword);
+var generateBtn = document.querySelector("#generate").addEventListener("click", writePassword);
 
 //Character bank array 
 var charsAll = {
@@ -11,10 +11,25 @@ var charsAll = {
   symbols: "!@#$%^&*_+=?",
 };
 
+// Variable Declaration 
+var confirmLength = "";
+var confirmUpperCase;
+var confirmLowerCase;
+var confirmNumbers;
+var confirmSymbols;
+
 // user criteria prompts: length and type
 function writePassword() {
   // PROMPT:  How long is this password?
-  var length = (prompt)("Please enter the numeric value required for your password's length, from 8-128 characters.");
+  var confirmLength = (prompt)("Please enter the numeric value required for your password's length, from 8-128 characters.");
+
+  //Error message loop if incorrect number selected.
+  while(confirmLength <= 7 || confirmLength >= 128) {
+    alert("I'm sorry. Password length must be between 8-128 characters. Please try again.");
+    var confirmLength = (prompt)("Please enter the numeric value required for your password's length, from 8-128 characters.");
+  } 
+
+
   // PROMPT: What kinds of characters should be included in this password? 
   const upper=  (prompt)("Does the password require uppercase letters?"); 
   const lower=  (prompt)("Does the password require lowercase letters?"); 
