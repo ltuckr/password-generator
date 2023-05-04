34ea177ee1caba7cae3801aeee56cf4f74eacee7
ltuckr <l.tucker.rdh@gmail.com>
2023-05-04 10:37:21 -0400

    added char array, added notation

--- script.js
+++ script.js
@@ -1,17 +1,25 @@
 // array of password creation character options// 
 
 
-var generateBtn = document.querySelector("#generate");
+var generateBtn = document.querySelector("#generate").addEventListener("click",writePassword);
+
+//Character array
+var charsAll = {
+  uppercase: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+  lowercase: "abcdefghijklmnopqrstuvwxyz",
+  numbers: "1234567890",
+  symbols: "!@#$%^&*_+=?",
+};
 
 // user criteria prompts: length and type
 function writePassword() {
   // PROMPT:  How long is this password?
-  var Length = (prompt)("Please enter the numeric value required for your password's length, from 8-128 characters.");
+  var length = (prompt)("Please enter the numeric value required for your password's length, from 8-128 characters.");
   // PROMPT: What kinds of characters should be included in this password? 
   var upper=  (prompt)("Does the password require uppercase letters?"); 
   var lower=  (prompt)("Does the password require lowercase letters?"); 
   var numbers=  (prompt)("Does the password require numbers?"); 
-  var symbols=  (prompt)("Does the password require symbols"); 
+  var symbols=  (prompt)("Does the password require symbols?"); 
 
 
   var password = generatePassword();
@@ -20,17 +28,9 @@ function writePassword() {
 }
 
 function generatePassword() {
-  var charsAll = {
-    uppercase: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
-    lowercase: "abcdefghijklmnopqrstuvwxyz",
-    numbers: "1234567890",
-    symbols: "!@#$%^&*_+=?",
-  };
-  var charSet = charsAll[passChars.toUpperCase()];
-  var retVal= "";
-  for (i = 0; i < passLength; i++)
-  retVal += charSet.charAt(Math.floor(Math.random() * charSet.passLength));
-  return retVal;
+ 
+
+
 
   
 
