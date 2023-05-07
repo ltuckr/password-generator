8f0aec1bf18cc5cd2b60f923e7f46b024ba81b50
ltuckr <l.tucker.rdh@gmail.com>
2023-05-07 18:13:49 -0400

    corrected mistitled function

--- script.js
+++ script.js
@@ -19,28 +19,32 @@ var confirmNumbers;
 var confirmSymbols;
 
 // user criteria prompts: length and type
-function createPassword() {
+function generatePassword() {
   // PROMPT:  How long is this password?
   var confirmLength = (prompt)("Please enter the numeric value required for your password's length, from 8-128 characters.");
+
   //Error message for loop.
-  while (confirmLength <= 7 || confirmLength >= 128);
+  while (confirmLength <= 7 || confirmLength >= 128);{
     alert("I'm sorry. Password length must be between 8-128 characters. Please try again.");
     var confirmLength = (prompt)("Please enter the numeric value required for your password's length, from 8-128 characters.");
-};
+  }
+
   // PROMPT: What kinds of characters should be included in this password? 
   var confirmUpper = confirm("Does the password require uppercase letters?");
   var confirmLower = confirm("Does the password require lowercase letters?");    
   var confirmNumbers= confirm("Does the password require numbers?");
   var confirmSymbols = confirm("Does the password require symbols?");
-    
+
+
     //Error message and for loop and repeating prompts if at least one criteria is not selected.
-  while(confirmUpper=== false && confirmLower === false && confirmNumbers === false && confirmSymbols === false) {
-    alert("Please choose at least one criteria for your password and try again");
-    var confirmUpper = confirm("Does the password require uppercase letters?");
-    var confirmLower = confirm("Does the password require lowercase letters?");    
-    var confirmNumbers= confirm("Does the password require numbers?");
-    var confirmSymbols = confirm("Does the password require symbols?");
-  }
+    while(confirmUpper === false && confirmLower === false && confirmNumbers === false && confirmSymbols === false) {
+      alert("Please choose at least one criteria for your password and try again");
+      confirmUpper = confirm("Does the password require uppercase letters?");
+      confirmLower = confirm("Does the password require lowercase letters?");    
+      confirmNumbers= confirm("Does the password require numbers?");
+      confirmSymbols = confirm("Does the password require symbols?");
+    }
+  
 
   
   // Now, let's make this password.
@@ -71,21 +75,27 @@ function createPassword() {
 
   for (var i = 0; i < confirmLength; i++); {
     var Selections = userSelect[Math.floor(Math.random() * userSelect.length)];
-    password.push(Selections);
+    password.push(Selections)
   }
 
-    
-  generateBtn.addEventListener("click", writePassword);
-  // Write password to the #password input
-  function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
+  return password.join("");
 
 }
+
 // Add event listener to generate button
 var generateBtn = document.querySelector("#generate")
+
+generateBtn.addEventListener("click", writePassword);
+// Write password to the #password input
+function writePassword() {
+var password = generatePassword();
+var passwordText = document.querySelector("#password");
+
+passwordText.value = password;
+
+}
+
+}
 
 
     
